2d0891001c15d43f247c5998210f5d6c0dd4428c
Eric Eichhorn <ee.eric.eichhorn@outlook.de>
2019-06-10 18:28:59 +0200

    PWA :)

---
+++ dist/precache-manifest.6d6dc80e24296b38b13bd0f41b8a7b69.js
@@ -0,0 +1,28 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "42e1c91573e0327cd3f17f251e8af2a6.mp3"
+  },
+  {
+    "url": "43075ffb3d6eafe5a17341ce79d9eb1d.png"
+  },
+  {
+    "url": "4548616750b49599e29ad31f27052fc7.mp3"
+  },
+  {
+    "url": "4a2d1fef95cd91bc66caa315b22be2dd.png"
+  },
+  {
+    "url": "689f34416f2a30b778e18f523a068af4.png"
+  },
+  {
+    "url": "f1ceea5d555947c80281f368bfaf8fa8.mp3"
+  },
+  {
+    "revision": "64343941badc14464a522faad8389201",
+    "url": "index.html"
+  },
+  {
+    "revision": "a91fbfe9bc582f56cad2",
+    "url": "main.js"
+  }
+]);
--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,14 @@ function main() {
     var app = new App();
     app.run();
 }
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
+            .then(registration => {
+                console.log('SW registered: ', registration);
+            }).catch(registrationError => {
+                console.log('SW registration failed: ', registrationError);
+            });
+    });
+}
