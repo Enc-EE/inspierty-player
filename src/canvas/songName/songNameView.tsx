1b98debcf3700c188619bff012abc6f0606960d0
Eric Eichhorn <ee.eric.eichhorn@outlook.de>
2019-11-05 19:05:46 +0100

    improved songname changing

--- src/canvas/songName/songNameView.tsx
+++ src/canvas/songName/songNameView.tsx
@@ -12,67 +12,99 @@ export interface StateProps {
 type Props = StateProps
 
 export interface State {
-    isAnimating: boolean
-    songName: string
-    nextSongName: string | null
+    isFadeOut: boolean
+    songName1: string | null
+    songName2: string | null
+    isShowingSongName1: boolean
 }
 
 class SongNameComponent extends React.Component<Props, State> {
-    private songNameDomElement: HTMLSpanElement | null
+    private songNameDomElement1: HTMLSpanElement | null
+    private songNameDomElement2: HTMLSpanElement | null
 
     constructor(props: Props) {
         super(props)
-        this.songNameDomElement = null
+        this.songNameDomElement1 = null
+        this.songNameDomElement2 = null
         this.state = {
-            isAnimating: false,
-            nextSongName: null,
-            songName: props.songName
+            isFadeOut: false,
+            songName1: this.props.songName,
+            songName2: null,
+            isShowingSongName1: true,
         }
     }
 
-    private animationEnded = (ev: TransitionEvent) => {
-        if (this.state.isAnimating && this.songNameDomElement && this.songNameDomElement.classList.contains('animate')) {
+    private animationEnded1 = (ev: TransitionEvent) => {
+        if (this.state.isFadeOut && this.state.isShowingSongName1) {
             this.setState({
                 ...this.state,
-                songName: this.state.nextSongName ? this.state.nextSongName : "",
-                isAnimating: false,
-                nextSongName: null,
+                isFadeOut: false,
+                isShowingSongName1: !this.state.isShowingSongName1,
+                songName1: null,
+            })
+        }
+    }
+
+    private animationEnded2 = (ev: TransitionEvent) => {
+        if (this.state.isFadeOut && !this.state.isShowingSongName1) {
+            this.setState({
+                ...this.state,
+                isFadeOut: false,
+                isShowingSongName1: !this.state.isShowingSongName1,
+                songName2: null,
             })
         }
     }
 
     componentDidMount() {
-        if (this.songNameDomElement) {
-            this.songNameDomElement.addEventListener('transitionend', this.animationEnded)
+        if (this.songNameDomElement1) {
+            this.songNameDomElement1.addEventListener('transitionend', this.animationEnded1)
+        }
+        if (this.songNameDomElement2) {
+            this.songNameDomElement2.addEventListener('transitionend', this.animationEnded2)
         }
     }
 
     componentDidUpdate(prevProps: StateProps) {
-        if (this.state.isAnimating) {
-            if (this.props.songName != this.state.nextSongName) {
+        if (this.state.isFadeOut) {
+            if (this.state.isShowingSongName1
+                ? this.state.songName2 != this.props.songName
+                : this.state.songName1 != this.props.songName) {
                 this.setState({
                     ...this.state,
-                    nextSongName: this.props.songName,
+                    songName1: this.state.isShowingSongName1 ? this.state.songName1 : this.props.songName,
+                    songName2: !this.state.isShowingSongName1 ? this.state.songName2 : this.props.songName,
                 })
             }
-        } else if (this.state.songName != this.props.songName) {
+        } else if (this.state.isShowingSongName1
+            ? this.state.songName1 != this.props.songName
+            : this.state.songName2 != this.props.songName) {
             this.setState({
                 ...this.state,
-                isAnimating: true,
-                nextSongName: this.props.songName,
+                isFadeOut: true,
+                songName1: this.state.isShowingSongName1 ? this.state.songName1 : this.props.songName,
+                songName2: !this.state.isShowingSongName1 ? this.state.songName2 : this.props.songName,
             })
         }
     }
 
     render() {
-        var className = "song-name"
-
-        if (this.state.isAnimating) {
-            className += " animate"
+        var classNameSongName1 = "song-name"
+        var classNameSongName2 = "song-name"
+        if (this.state.isFadeOut) {
+            classNameSongName1 += " hide"
+            classNameSongName2 += " hide"
+        } else if (this.state.isShowingSongName1) {
+            classNameSongName2 += " hide"
+        } else if (!this.state.isShowingSongName1) {
+            classNameSongName1 += " hide"
         }
 
         return (
-            <span style={{ top: this.props.height * 0.6 + "px" }} ref={c => this.songNameDomElement = c} className={className}>{this.state.songName}</span>
+            <div>
+                <span style={{ top: this.props.height * 0.6 + "px" }} ref={c => this.songNameDomElement1 = c} className={classNameSongName1}>{this.state.songName1}</span>
+                <span style={{ top: this.props.height * 0.6 + "px" }} ref={c => this.songNameDomElement2 = c} className={classNameSongName2}>{this.state.songName2}</span>
+            </div>
         )
     }
 }
