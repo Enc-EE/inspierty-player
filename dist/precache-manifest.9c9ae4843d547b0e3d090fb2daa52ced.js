9e14801cfd206949e8d6f4df462bf6c16140e456
Eric Eichhorn <ee.eric.eichhorn@outlook.de>
2019-11-05 23:49:27 +0100

    build

---
+++ dist/precache-manifest.9c9ae4843d547b0e3d090fb2daa52ced.js
@@ -0,0 +1,60 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "3571a1525a5231c2e0766f71fdfd8dff.png"
+  },
+  {
+    "url": "42e1c91573e0327cd3f17f251e8af2a6.mp3"
+  },
+  {
+    "url": "4548616750b49599e29ad31f27052fc7.mp3"
+  },
+  {
+    "url": "5a7098585e7994a81124d5465e3472ba.png"
+  },
+  {
+    "url": "9fbf4088a7b1e840300cea4f3053fdb3.png"
+  },
+  {
+    "revision": "08f58f7678b545de5960f4ed80d37158",
+    "url": "assets\\static\\DejaVuSerif-Italic.ttf"
+  },
+  {
+    "revision": "c903990d451526897103579ae219aeb2",
+    "url": "assets\\static\\icon-128x128.png"
+  },
+  {
+    "revision": "ce17d82b81a7e13d7ef0080c5eb63a1f",
+    "url": "assets\\static\\icon-144x144.png"
+  },
+  {
+    "revision": "cdbeeafa74d7f964ca710067f2142228",
+    "url": "assets\\static\\icon-152x152.png"
+  },
+  {
+    "revision": "5c2bcc045c5b0f07bf0317973e2869bf",
+    "url": "assets\\static\\icon-192x192.png"
+  },
+  {
+    "revision": "7724fe7738355600fe876b2af987697f",
+    "url": "assets\\static\\icon-256x256.png"
+  },
+  {
+    "revision": "c2a89fd27be54f5b34c03de58dcb4282",
+    "url": "assets\\static\\icon-512x512.png"
+  },
+  {
+    "url": "f1ceea5d555947c80281f368bfaf8fa8.mp3"
+  },
+  {
+    "revision": "963915d84e6f46c1431a9e195a8a56f8",
+    "url": "index.html"
+  },
+  {
+    "revision": "7da812b44c9500bf7f10",
+    "url": "main.js"
+  },
+  {
+    "revision": "1863bc3d947f3a926ffc80c2a707d32f",
+    "url": "manifest.json"
+  }
+]);
